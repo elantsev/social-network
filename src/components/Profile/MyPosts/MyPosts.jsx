f2f083b1831881ff17319bb105b2eeeaac4168d0
Павел <elantsev.pa@gmail.com>
2019-11-02 18:45:03 +0300

    MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,16 +3,31 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = props => {
+  let postData = [
+    { id: 1, message: "Hi, how are you?", likesCount: 12 },
+    { id: 2, message: "It's my first post", likesCount: 11 }
+  ];
+
   return (
-    <div>
-      My posts
+    <div className={s.postsBlock}>
+      <h3>My posts</h3>
       <div>
-        <textarea></textarea>
-        <button>Add post</button>
+        <div>
+          <textarea></textarea>
+        </div>
+        <div>
+          <button>Add post</button>
+        </div>
       </div>
       <div className={s.posts}>
-        <Post message="I am first post" likesCount="5" />
-        <Post message="Hi" likesCount="13" />
+        <Post
+          message={postData[0].message}
+          likesCount={postData[0].likesCount}
+        />
+        <Post
+          message={postData[1].message}
+          likesCount={postData[1].likesCount}
+        />
       </div>
     </div>
   );
