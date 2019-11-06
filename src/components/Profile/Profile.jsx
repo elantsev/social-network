16583a65a4975ce554ff1a1ec20b2ec8c5297884
Павел <elantsev.pa@gmail.com>
2019-11-06 22:03:30 +0300

    38 - dispatch, action

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -10,8 +10,8 @@ const Profile = props => {
       <MyPosts
         posts={props.profilePage.posts}
         newPostText={props.profilePage.newPostText}
-        updateNewPostText={props.updateNewPostText}
-        addPost={props.addPost}
+        dispatch={props.dispatch}
+      />
       />
     </div>
   );
