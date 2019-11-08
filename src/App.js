7c7846ddba16633ef2d7ccd2c40cdf23c89d1828
Павел <elantsev.pa@gmail.com>
2019-11-08 15:56:20 +0300

    40 - add message from dialogs page

--- src/App.js
+++ src/App.js
@@ -11,10 +11,7 @@ const App = props => {
       <Header />
       <Navbar />
       <div className="app-wrapper-content">
-        <Route
-          path="/dialogs"
-          render={() => <Dialogs state={props.state.dialogsPage} />}
-        />
+        <Route path="/dialogs" render={() => <Dialogs store={props.store} />} />
         <Route
           path="/profile"
           render={() => (
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -12,7 +12,6 @@ const Profile = props => {
         newPostText={props.profilePage.newPostText}
         dispatch={props.dispatch}
       />
-      />
     </div>
   );
 };
