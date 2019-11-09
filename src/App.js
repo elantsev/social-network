efe9516f2795259732e3dfa2e548cfa0af483e4a
Павел <elantsev.pa@gmail.com>
2019-11-09 16:31:00 +0300

    49 - users page reducer and components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
 
-const App = props => {
+const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
@@ -14,6 +15,7 @@ const App = props => {
       <div className="app-wrapper-content">
         <Route path="/dialogs" render={() => <DialogsContainer />} />
         <Route path="/profile" render={() => <Profile />} />
+        <Route path="/users" render={() => <UsersContainer />} />
       </div>
     </div>
   );
