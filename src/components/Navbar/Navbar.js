fe6bb3075cd7f26af36d5f894bd3bdfbd9b5efb3
Павел <elantsev.pa@gmail.com>
2019-11-10 11:42:26 +0300

    54: componentDidMount

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,6 +15,11 @@ const Navbar = () => {
           Messages
         </NavLink>
       </div>
+      <div className={`${s.item} ${s.active}`}>
+        <NavLink to="/users" activeClassName={s.activeLink}>
+          Users
+        </NavLink>
+      </div>
       <div className={s.item}>
         <NavLink to="/news" activeClassName={s.activeLink}>
           News
