9f0acb190e57ef9d83c1d6347b8f892d2c0d3d9d
elantsev <elantsev.pa@gmail.com>
2019-12-25 21:02:35 +0300

    .

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
-        "API-KEY": "b1775b2f-c3a5-4509-8dc9-90b5629de7c3"
+        "API-KEY": "a77471f5-9e91-4879-aa89-ad0aa2e8cdb4"
     }
 });
 
