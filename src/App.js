42e98051cfa1b3f076a23a80c92d58e9cf3ca090
elantsev <elantsev.pa@gmail.com>
2020-01-05 22:46:12 +0300

    68: redirect

--- src/App.js
+++ src/App.js
@@ -6,16 +6,18 @@ import ProfileContainer from "./components/Profile/ProfileContainer";
 import { Route } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
+import LoginPage from "./components/Login/Login";
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <HeaderContainer  />
+      <HeaderContainer />
       <Navbar />
       <div className="app-wrapper-content">
         <Route path="/dialogs" render={() => <DialogsContainer />} />
         <Route path="/profile/:userId?" render={() => <ProfileContainer />} />
         <Route path="/users" render={() => <UsersContainer />} />
+        <Route path='/login' render={() => <LoginPage />} />
       </div>
     </div>
   );
